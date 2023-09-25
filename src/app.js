5cab31b077fc74e755c7016f1333aac3d1702671
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-09-25 18:25:22 -0400

    finished domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
+
+let pronoun = ['The', 'Our'];
+
+let adjectives = [
+  'Beautiful',
+  'Mysterious',
+  'Enchanting',
+  'Courageous',
+  'Vibrant',
+  'Serene',
+  'Playful',
+  'Radiant',
+  'Tranquil',
+  'Majestic',
+];
+
+let nouns = [
+  'Ocean',
+  'Mountain',
+  'Sunset',
+  'Adventure',
+  'Friendship',
+  'Dream',
+  'Journey',
+  'Castle',
+  'Forest',
+  'Star',
+];
+
+const randomElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const domain = document.querySelector('#domain');
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  domain.innerHTML = `${randomElement(pronoun)}${randomElement(
+    adjectives
+  )}${randomElement(nouns)}.com`;
+  console.log(domain.innerHTML);
+  console.log('Hello Rigo from the console!');
 };
